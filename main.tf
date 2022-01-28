a02964998ba7cf495ace3b74bb579695ddf96d1b
Rohan Gupta <75747077+rggitops@users.noreply.github.com>
2022-01-28 12:53:33 +0530

    removing the subnet part

--- main.tf
+++ main.tf
@@ -33,7 +33,6 @@ resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  subnet_id              = "subnet-0aa307464b07d2d6e"
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, Rohan Gupta" > index.html
